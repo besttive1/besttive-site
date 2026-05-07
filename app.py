8ecb97943f330b29bd161b18d38f140dd98a4556
Amit <amitjia2000@gmail.com>
2026-05-07 17:01:42 +0530

    fix helper indentation

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "amitjia2000@gmail.com"
-app.config['MAIL_PASSWORD'] = ""
+app.config['MAIL_PASSWORD'] = "qqcvybwavghvijgp"
 mail = Mail(app)
 
 # --------- MODEL ---------
